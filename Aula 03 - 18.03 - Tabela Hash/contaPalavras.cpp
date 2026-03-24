4be634909ce1288c4abaa9c96d47c218add9133a
Diwei <diwei.dvn@gmail.com>
2026-03-24 15:25:47 -0300

    aula03 update

--- Aula 03 - 18.03 - Tabela Hash/contaPalavras.cpp
+++ Aula 03 - 18.03 - Tabela Hash/contaPalavras.cpp
@@ -1,61 +1,51 @@
-#include <iostream>
 #include <cctype>
 #include <fstream>
+#include <iostream>
 #include <vector>
 
 using namespace std;
 
-struct wordcount{
-    string word; 
-    int count;
+struct wordcount {
+  string word;
+  int count;
 };
 
-int main(){
+int main() {
+  ifstream arq("clarissa.txt");
+  // TODO: testar se abriu com sucesso
+  char ch;
+  string word;
+  int nchar = 0, nwords = 0;
+  vector<wordcount> distinctwords;
 
-    ifstream arq("gpl3.txt");
-    // TODO: testar se abriu com sucesso
-    if(!arq){
-        cout << "Não abriu" << endl;
-        exit(1);
-    }
-
-    char ch;
-    string word;
-    int nchar=0, nwords=0;
-    vector<wordcount> distinctwords;
-
-    while(arq.get(ch)){              // Lê um caracter por vez  
-        nchar++;               
-        if(isalpha(ch)){            // "isalpha siginifica "é letra"
-            word += tolower(ch);    // adiciona LETRA no word se for LETRA | tolower transforma tudo em minusculo
+  while (arq.get(ch)) { // Lê um caracter por vez
+    nchar++;
+    if (isalpha(ch)) {       // É letra?
+      word += tolower(ch);   // Concatena em minúsculo
+    } else if (word != "") { // Não é letra e existe uma palavra
+      bool found = false;
+      for (auto &wc : distinctwords) { // para cada palavra contida no vector
+        if (wc.word == word) {         // se já existe
+          wc.count++;                  // incrementa contagem
+          found = true;
+          break;
         }
-        else if(!word.empty()){     // se word não estiver vazio(estiver com letra)
-            bool found = false;
-            for(auto &wc : distinctwords){   //para cada palavra diferente
-                if(wc.word == word){          // se já existe
-                    wc.count++;             //incrementa contagem
-                    found = true;
-                    break;
-                }
-            }
-            if(!found){             // não achou
-                distinctwords.push_back({word, 1}); //insere na struct palavra e contador
-            }
-            word.clear();
-            nwords++;           // limpa word
-        }
+      }
+      if (!found) {                         // Não achou
+        distinctwords.push_back({word, 1}); // Insere com count=1
+      }
+      word = ""; // Limpa
+      nwords++;
     }
-    arq.close();
+  }
+  arq.close();
+  cout << nchar << " caracteres\n"
+       << nwords << " palavras\n"
+       << distinctwords.size() << " distintas\n";
 
-    cout << nchar << " caracteres\n"
-         << nwords << " palavras\n"
-         << distinctwords.size() << " distintas\n";
-         
-    ofstream csv("dados.csv");
-    for(auto &wc : distinctwords){
-        csv << "\"" << wc.word << "\";" << wc.count << endl; 
-    }
-    csv.close();
-
-    return 0;
+  ofstream csv("dados.csv");
+  for(auto &wc : distinctwords){
+    csv << "\"" << wc.word << "\";" << wc.count << "\n";
+  }
+  csv.close();
 }
