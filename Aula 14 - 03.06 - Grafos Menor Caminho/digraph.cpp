4b0c1ec5cf1c18f756d9e847d7be91dc58f423da
Diwei LCI <diwei.dvn@gmail.com>
2026-06-03 21:47:17 -0300

    Aula 14 - 03.06 incompleta

---
+++ Aula 14 - 03.06 - Grafos Menor Caminho/digraph.cpp
@@ -0,0 +1,172 @@
+#include <algorithm>
+#include <unordered_map>
+#include <unordered_set>
+#include <fstream>
+#include <iostream>
+#include <cstdlib>
+#include <string>
+#include <queue>
+#include <vector>
+
+namespace UPF {
+
+template <typename T>
+class digraph {
+private:
+    struct node {
+        T value;
+        std::unordered_set<node*> links;
+    };
+
+    std::unordered_map<T, node> graph;
+
+    node* find(const T& val)
+    {
+        auto it = graph.find(val);
+        if(it==graph.end()){ // Not found!
+            return nullptr;
+        }
+        return &it->second;
+    }
+
+    void visit_node(node *p, std::unordered_set<node *> &visited, int level){
+        std::cout << std::string(level, '\t')<< p->value << "\n";
+        visited.insert(p);
+        for(auto adj: p->links){
+            if(visited.count(adj)==0){
+                visit_node(adj, visited, level+1);
+            }
+        }
+    }
+
+public:
+    void insert_node(const T& val)
+    {
+        if (graph.count(val) != 0)
+            return;
+        node aux;
+        aux.value = val;
+        graph[val] = aux;
+    }
+
+    void insert_link(const T& from, const T& to)
+    {
+        auto pfrom = find(from);
+        if(!pfrom) return;
+        auto pto = find(to);
+        if(!pto) return;
+
+        pfrom->links.insert(pto);
+    }
+
+    void show(){
+        std::ofstream dot("/tmp/g123456.dot");
+        dot << "digraph{\n";
+        for(const auto &[key, node] : graph){
+            dot << "\t\"" << key << "\" -> {";
+            for(const auto &link : node.links){
+                dot << "\"" << link->value << "\" ";
+            }
+            dot << "};\n";
+        }
+        dot << "}\n";
+        dot.close();
+        system("dot -Tx11 /tmp/g123456.dot");
+    }
+
+    void DFS_from(const T& from){
+        auto p = find(from);
+        if(!p) return;
+        std::unordered_set<node *> visited;
+        visit_node(p, visited, 0);
+    }
+
+    void save(const std::string &filename){
+        std::ofstream out(filename);
+        out << graph.size() << "\n";
+        for (const auto& [key, node] : graph){
+            out << node.value << "\n";
+        }
+        for (const auto& [key,node] : graph){
+            for(const auto& link : node.links){
+                out << key << '\n';
+                out << link->value << '\n';
+            }
+        }
+        out.close();
+    }
+
+    void shortest_path(const T& start, const T& end){
+
+        std::unordered_set<node *> visited;
+        visit_node();
+
+        std::vector<node *> shor
+
+        auto pstart = find(start);
+        if(!pstart) return;
+
+        auto pend = find(end);
+        if(!pend) return;
+
+        std::queue<node *> q;
+        std::unordered_set<node *>queued;
+        std::unordered_map<node *, node *> origin;
+        q.push(pstart);
+        queued.insert(pstart);
+        origin[pstart] = nullptr;
+        bool found = false;
+        while(!q.empty()){
+            auto current = q.front();
+            q.pop();
+            if(current==pend) 
+                found = true;
+                break;
+            for(auto adj : current->links){
+                if(queued.count(adj)==0){
+                    q.push(adj);
+                    queued.insert(adj);
+                    origin[adj] = current;
+                }
+            }
+        }
+        if(found){
+            auto p = pend;
+            while(p){
+                path.push_back(p);
+                p = origin[p];
+            }
+        }
+        std::reverse(path.begin(), path.end());
+
+        return path;
+    }
+
+    void load(const std::string &filename){
+        std::ifstream in(filename);
+        if(!in) return; //Se não tem arquivo sai da função
+        int n;
+        in >> n; in.ignore();
+        std::string line, line2;
+        while(n--){  // enquanto n for true executa(qualquer numero que nao seja 0 é true)
+            getline(in,line);
+            insert_node(line);
+        }
+        while(getline(in,line) && getline(in, line2)){
+            insert_link(line, line2);
+        }
+
+        in.close();
+    }
+
+    void remove(const T &name){
+        auto pfrom = find(name);
+        if(!pfrom) return;
+        
+        for(auto& [key, node] : graph){
+            node.links.erase(pfrom);
+        }
+        graph.erase(name);
+    }
+};
+}
---
+++ Aula 14 - 03.06 - Grafos Menor Caminho/menu.cpp
@@ -0,0 +1,120 @@
+#include "digraph.cpp"
+#include <cctype>
+#include <fstream>
+#include <iostream>
+#include <string>
+
+using namespace std;
+
+char menu()
+{
+    char opcao;
+    while (true) {
+        cout << "[\x1b[31mI\x1b[0mncluir] "
+             << "[\x1b[31mE\x1b[0mxcluir] "
+             << "[Seguir \x1b[31mU\x1b[0msuário] "
+             << "[\x1b[31mD\x1b[0mFS] "
+             << "[\x1b[31mC\x1b[0maminho]"
+             << "[Exibir \x1b[31mR\x1b[0mede] "
+             << "[\x1b[31mS\x1b[0mair] > ";
+        cin >> opcao;
+        cin.ignore(); 
+        opcao = toupper(opcao);
+        if (opcao == 'C' || opcao == 'I' || opcao == 'E' || opcao == 'U' || opcao == 'D' || opcao == 'R' || opcao == 'S') {
+            return opcao;
+        }
+    }
+    cout << "Opção inválida. Tente novamente.\n";
+}
+
+void add_user(UPF::digraph<string>& rede)
+{
+    cout << "Nome do usuário: ";
+    string name;
+    getline(cin, name);
+    rede.insert_node(name);
+}
+
+void follow_user(UPF::digraph<string>& rede)
+{
+    string from, to;
+    cout << "Nome do seguidor: ";
+    getline(cin, from);
+    cout << "Quem irá seguir: ";
+    getline(cin, to);
+    rede.insert_link(from, to);
+}
+
+void remove_user(UPF::digraph<string>& rede)
+{
+    cout << "Nome do usuário: ";
+    string name;
+    getline(cin, name);
+    rede.remove(name);
+}
+
+void user_dfs(UPF::digraph<string>& rede)
+{
+    cout << "Usuário inicial: ";
+    string name;
+    getline(cin, name);
+    rede.DFS_from(name);
+}
+
+void path(UPF::digraph<string>& rede)
+{
+    string start, end;
+    cout << "Inicial: ";
+    getline(cin, start);
+    cout << "Final: ";
+    getline(cin, end);
+    auto path = rede.shortest_path(start, end);
+    if(path.empty()){
+        std::cout << "Nenhum caminho encontrado\n";
+    }
+    else{
+        for(auto p : path)
+            std::cout << p->value << " ";
+        std::cout << "\n";
+    }
+}
+
+int main()
+{
+    UPF::digraph<string> rede;
+    char opcao;
+
+    rede.load("rede.txt");
+    do {
+        opcao = menu();
+
+        switch (opcao) {
+        case 'I':
+            add_user(rede);
+            break;
+        case 'E':
+            remove_user(rede);
+            break;
+        case 'U':
+            follow_user(rede);
+            break;
+        case 'D':
+            user_dfs(rede);
+            break;
+        case 'R':
+            rede.show();
+            break;
+        case 'S':
+            break;
+        case 'C':
+            path(rede);
+            break;
+        default:
+            cout << "Opção inválida. Tente novamente.\n";
+            break;
+        }
+    } while (opcao != 'S');
+
+    rede.save("rede.txt");
+    return 0;
+}
